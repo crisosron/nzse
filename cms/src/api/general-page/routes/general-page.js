ef2b31723a780928facf6f857675c7b7f4391fc2
Ron Crisostomo <ron@hauntdigital.co.nz>
2022-07-03 15:27:57 +1200

    Add scaffolding for CardBlock component

---
+++ cms/src/api/general-page/routes/general-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * general-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::general-page.general-page');
