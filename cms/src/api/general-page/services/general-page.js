599d2759c332caa4debc2377b0ea40d1582d2830
Ron Crisostomo <ron@hauntdigital.co.nz>
2022-07-10 15:35:33 +1200

    Add general page

---
+++ cms/src/api/general-page/services/general-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * general-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::general-page.general-page');
