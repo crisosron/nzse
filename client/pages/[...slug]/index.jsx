b60182b5e5d6eecbc18f9cd206aeba23b200cd05
Ron Crisostomo <ron@hauntdigital.co.nz>
2022-07-23 21:17:06 +1200

    Add TODOs on general page

--- client/pages/[...slug]/index.jsx
+++ client/pages/[...slug]/index.jsx
@@ -5,6 +5,8 @@ import { formatDate, unwrapCollectionEntityResponse } from "../../lib/utils";
 import Layout from "../../components/layout";
 import { Blocks } from "../../components/blocks";
 
+// TODO: Should this apply only for non member-only pages? Because with member only pages, we need
+// to do some client side checks to determine if the user is logged in or not?
 export const getStaticPaths = async () => {
   const slugs = unwrapCollectionEntityResponse(
     await graphqlClient.query({ query: getAllGeneralPageSlugs }), 'generalPages'
@@ -16,12 +18,16 @@ export const getStaticPaths = async () => {
   }
 };
 
+// TODO: How to do logged in check for member only pages? Ideally we return a 404 here at some point
+// If not possible here in getStaticProps, resort to doing the check in the client side (where the
+// component code is below), and render the error page manually.
 export const getStaticProps = async ({ params }) => {
   const { slug } = params;
   const generalPageBySlugQuery = buildGeneralPageBySlugQuery(slug);
   const queryResponse = await graphqlClient.query({ query: generalPageBySlugQuery });
   const generalPages = unwrapCollectionEntityResponse(queryResponse, 'generalPages');
   const generalPage = generalPages.length ? generalPages[0] : null;
+
   return {
     props: generalPage
   }
