c8ad5e9287b7e59110f1e7642abe6cdced9b6ea9
Ron Crisostomo <rcrisostomo97@gmail.com>
2022-09-18 16:11:51 +1200

    Update sidebar styling

--- client/components/layout.jsx
+++ client/components/layout.jsx
@@ -1,7 +1,7 @@
 import Nav from "./nav";
 
 const Layout = ({ children, categories, seo }) => (
-  <div className="font-sansation mx-5 lg:mx-80 xl:mx-96">
+  <div className="Layout font-sansation mx-5 lg:mx-80 flex justify-center">
     <Nav categories={categories} />
     {children}
   </div>
--- client/components/sidebar.jsx
+++ client/components/sidebar.jsx
@@ -1,8 +1,9 @@
 import Blocks from './blocks/blocks';
 
-const Sidebar = ({ navBlocks }) => {
+const Sidebar = ({ navBlocks, className }) => {
+  console.log('Sidebar props className', className)
   return (
-    <div className="lg:py-16 lg:pr-4 border-r-2 border-gray">
+    <div className={`Sidebar lg:py-16 lg:pr-4 border-r-2 border-gray md:w-40 lg:w-48 md:flex md:justify-center ${className}`}>
       <Blocks blocks={navBlocks} />
     </div>
   );
