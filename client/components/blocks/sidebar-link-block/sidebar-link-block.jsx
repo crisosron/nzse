4420b4f3adca2e05c364ad47deb768e82686051e
Ron Crisostomo <rcrisostomo97@gmail.com>
2022-09-24 16:50:00 +1200

    Add sidebar link highlighting of current page

--- client/components/blocks/sidebar-link-block/sidebar-link-block.jsx
+++ client/components/blocks/sidebar-link-block/sidebar-link-block.jsx
@@ -1,4 +1,5 @@
 import { unwrapEntityResponse } from "../../../lib/utils";
+import { useRouter } from 'next/router';
 import Link from 'next/link'
 
 const SidebarLinkBlock = (props) => {
@@ -11,11 +12,14 @@ const SidebarLinkBlock = (props) => {
     membersOnly: pageMembersOnly
   } = unwrapEntityResponse(page);
 
+  const router = useRouter()
   const audienceRoute = pageAudience === "Patients" ? "patients" : "professionals";
+  const pathComponents = router.asPath.split('/')
+  const representsCurrentRoute = pathComponents[pathComponents.length - 1] === pageSlug
 
   return (
     <Link href={`/${audienceRoute}/${pageSlug}`}>
-      <a className="inline-block text-gray mb-4"> {title || pageTitle} </a>
+      <a className={`inline-block mb-4 ${representsCurrentRoute ? 'text-light-blue-700' : 'text-gray'}`}>{title || pageTitle}</a>
     </Link>
   )
 };
