44cc9757945b6d2fc1c9fa04a9f66271377d65a3
Ron Crisostomo <rcrisostomo97@gmail.com>
2022-09-24 18:15:11 +1200

    Start dropdown sidebar item styling

--- client/components/blocks/sidebar-dropdown-block/sidebar-dropdown-block.jsx
+++ client/components/blocks/sidebar-dropdown-block/sidebar-dropdown-block.jsx
@@ -1,11 +1,20 @@
 import { unwrapEntityResponse } from "../../../lib/utils";
 
+const ChevronRight = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="h-8 w-8 fill-gray">
+      <path d="m18.75 36-2.15-2.15 9.9-9.9-9.9-9.9 2.15-2.15L30.8 23.95Z" />
+    </svg>
+  )
+}
+
 const SidebarDropdownBlock = ({ sidebarDropdownTitle: title, pages: pagesData }) => {
   const pages = pagesData.data.map(pageData => unwrapEntityResponse(pageData).attributes);
   
   return (
-    <div>
-      SidebarDropdownBlock
+    <div className="flex items-center">
+      <span className="inline-block text-gray">{title}</span>
+      <ChevronRight />
     </div>
   );
 }
