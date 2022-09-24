1d8eb95ac2797e56f373e5a728a98811ec45bde1
Ron Crisostomo <rcrisostomo97@gmail.com>
2022-09-24 15:13:12 +1200

    Add color tints for light-blue and gray

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -18,10 +18,33 @@ module.exports = {
     },
     colors: {
       'light-blue': '#4cbedb',
+      'light-blue-900': '#5ec4df',
+      'light-blue-800': '#70cbe2',
+      'light-blue-700': '#82d1e6',
+      'light-blue-600': '#94d8e9',
+      'light-blue-500': '#a6deed',
+      'light-blue-400': '#b7e5f1',
+      'light-blue-300': '#c9ebf4',
+      'light-blue-200': '#dbf2f8',
+      'light-blue-100': '#edf8fb',
+
       'lightest-blue': '#54cceb',
+
       'dark-blue': '#25235c',
+
       'link-blue': '#0074cc',
+
       'gray': '#7f8487',
+      'gray-900': '#8c9093',
+      'gray-800': '#999d9f',
+      'gray-700': '#a5a9ab',
+      'gray-600': '#b2b5b7',
+      'gray-500': '#bfc2c3',
+      'gray-400': '#cccecf',
+      'gray-300': '#d9dadb',
+      'gray-200': '#e5e6e7',
+      'gray-100': '#f2f3f3',
+      
       'white': '#FFFFFF'
     }
   },
