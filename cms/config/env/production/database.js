02c6a2ef7460ceb1ebc1458192b6db58cd9acf3c
Ron Crisostomo <ron@hauntdigital.co.nz>
2022-12-10 21:00:32 +1300

    Update prod db config

--- cms/config/env/production/database.js
+++ cms/config/env/production/database.js
@@ -1,36 +1,36 @@
-// const parse = require('pg-connection-string').parse;
-// const config = parse(process.env.DATABASE_URL);
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: config.host,
-//       port: config.port,
-//       database: config.database,
-//       user: config.user,
-//       password: config.password,
-//       ssl: {
-//         ca: env('DATABASE_CA')
-//       }
-//     },
-//     debug: false
-//   }
-// });
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
-        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
-      },
+        ca: env('DATABASE_CA')
+      }
     },
-    debug: false,
-  },
+    debug: false
+  }
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST'),
+//       port: env.int('DATABASE_PORT'),
+//       database: env('DATABASE_NAME'),
+//       user: env('DATABASE_USERNAME'),
+//       password: env('DATABASE_PASSWORD'),
+//       ssl: {
+//         rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+//       },
+//     },
+//     debug: false,
+//   },
+// });
