d4a2abc6e59e0a1c154e28fefb9b252518a69dfb
Ron Crisostomo <ron@hauntdigital.co.nz>
2022-12-11 13:31:18 +1300

    Disable graphql playground for env prod

--- cms/config/env/production/plugins.js
+++ cms/config/env/production/plugins.js
@@ -3,12 +3,11 @@ module.exports = ({ env }) => ({
     config: {
       endpoint: '/graphql',
       shadowCRUD: true,
-      playgroundAlways: true,
+      playgroundAlways: false,
       depthLimit: 20,
       amountLimit: 100,
       apolloServer: {
         tracing: false,
-        introspection: true,
       },
     },
   },
