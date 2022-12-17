02d8964453262d124f1a87d0bfb93cc90d6d8f79
Ron Crisostomo <ron@hauntdigital.co.nz>
2022-12-17 17:15:19 +1300

    Add Container to HeroBanner to control spacing

--- client/components/blocks/hero-banner-block/hero-banner-block.jsx
+++ client/components/blocks/hero-banner-block/hero-banner-block.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { unwrapEntityResponse } from '../../../lib/utils';
+import Container from '../../container';
 import ButtonBlock from '../button-block';
 
 // Note: heroBannerBlockTitle is the gql alias assigned to title to prevent field name conflicts
@@ -25,22 +26,24 @@ const HeroBannerBlock = ({
     // mechanisms implemented by NextJS (which is especially important in this case because its
     // a hero image). A mixture of absolute and relative positioning on divs are used to load the
     // image as a background image, without actually using css background image rules.
-    <div className='h-[80vh] relative font-poppins'>
+    <div className='HeroBannerBlock h-[80vh] relative font-poppins'>
       <div className='relative w-full h-full'>
         <Image src={imageUrl} alt={alternativeText} layout='fill' objectFit='cover' priority />
       </div>
-      <div className='absolute top-0 w-[80%] h-full flex leading-snug flex-col justify-end p-5 text-white z-10 md:justify-center lg:px-80'>
-        {preTitle && <span className='text-h3 md:text-h3-xl lg:text-h3-2xl'>{preTitle}</span>}
-        {title && (
-          <span className='text-h1 text-light-blue md:text-h1-xl lg:text-h1-2xl font-medium'>
-            {title}
-          </span>
-        )}
-        {subtitle && <span className='text-h5 md:text-h5-xl lg:text-h5-2xl'>{subtitle}</span>}
-        {ctaButtonProps && (
-          <ButtonBlock {...ctaButtonProps} className='max-w-[80%] text-center mb-0 mt-4' />
-        )}
-      </div>
+      <Container>
+        <div className='absolute top-0 w-[80%] h-full flex leading-snug flex-col justify-end py-5 md:py-0 text-white z-10 md:justify-center max-w-[75%] md:max-w-[100%]'>
+          {preTitle && <span className='block text-h3 md:text-h3-xl lg:text-h3-2xl'>{preTitle}</span>}
+          {title && (
+            <span className='block text-h1 text-light-blue md:text-h1-xl lg:text-h1-2xl font-medium'>
+              {title}
+            </span>
+          )}
+          {subtitle && <span className='block text-h5 md:text-h5-xl lg:text-h5-2xl'>{subtitle}</span>}
+          {ctaButtonProps && (
+            <ButtonBlock {...ctaButtonProps} className='max-w-[80%] text-center mb-0 mt-4' />
+          )}
+        </div>
+      </Container>
     </div>
   );
 };
