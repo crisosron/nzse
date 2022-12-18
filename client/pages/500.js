43e43cc0a7a93a468c73423316e0deb76eead75b
Ron Crisostomo <ron@hauntdigital.co.nz>
2022-12-18 14:12:01 +1300

    Add erro 500 page

---
+++ client/pages/500.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { Container, Layout } from "../components";
+const Error500 = ({ navigation: navigationData, footer: footerData }) => {
+  return (
+    <Layout navigationData={navigationData} footerData={footerData}>
+      <Container>
+        <div className='Error500 prose my-20 mx-auto w-full h-[54vh] flex justify-center items-center flex-col text-center'>
+          <h1>500</h1>
+          <p>It looks like something went wrong on our end.</p>
+          <p>
+            Try a different URL, or go back to the <Link href='/'>homepage</Link>.
+          </p>
+        </div>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Error500;
