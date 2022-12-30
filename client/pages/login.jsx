7cad988f21eab9acce8970b788cd98ef55218538
Ron Crisostomo <rcrisostomo97@gmail.com>
2022-12-30 17:07:53 +1300

    Update login page styling

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -1,6 +1,7 @@
 import { Container, Layout } from '../components';
 import { useForm } from 'react-hook-form';
 import { EMAIL_REGEX } from '../lib/form-utils';
+import Link from 'next/link';
 const LoginPage = ({ navigation: navigationData, footer: footerData }) => {
   const {
     register,
@@ -16,15 +17,16 @@ const LoginPage = ({ navigation: navigationData, footer: footerData }) => {
   return (
     <Layout navigationData={navigationData} footerData={footerData}>
       <Container>
-        <div className='LoginPage prose mx-auto w-full h-[82vh] md:h-[54vh] flex flex-col'>
+        <div className='LoginPage mx-auto w-full h-[82vh] md:h-[54vh] flex flex-col'>
           <div className='prose my-10 text-center w-full'>
-            <h1>Login</h1>
+            <h2>Member Login</h2>
           </div>
 
-          <div className='flex-grow mb-10 flex justify-center items-center text-center'>
+          <div className='prose flex-grow flex flex-col justify-center mb-10 text-center w-full'>
             <form onSubmit={handleSubmit(onSubmit)}>
-              <div className='block'>
+              <div className='block mb-5'>
                 <input
+                  className='border border-gray-300 rounded-md p-2 w-full'
                   type='text'
                   placeholder='Email Address'
                   {...register('emailAddress', { required: true, pattern: EMAIL_REGEX })}
@@ -32,8 +34,9 @@ const LoginPage = ({ navigation: navigationData, footer: footerData }) => {
                 {errors.emailAddress && <span>Please enter a valid email address</span>}
               </div>
 
-              <div className='block'>
+              <div className='block mb-5'>
                 <input
+                  className='border border-gray-300 rounded-md p-2 w-full'
                   type='password'
                   placeholder='Password'
                   {...register('password', { required: true })}
@@ -41,8 +44,17 @@ const LoginPage = ({ navigation: navigationData, footer: footerData }) => {
                 {errors.password && <span>Please enter your password</span>}
               </div>
 
-              <input type='Submit' defaultValue='Login' />
+              <input
+                className='bg-light-blue hover:bg-lightest-blue shadow hover:text-dark-blue text-white py-2 px-4 rounded transition-colors duration-150 w-[80%] mb-5'
+                type='Submit'
+                defaultValue='Login'
+              />
             </form>
+            <div>
+              <Link href='/reset-password'>
+                <a className='text-[14px] font-normal'>Forgot your password?</a>
+              </Link>
+            </div>
           </div>
         </div>
       </Container>
