29770c7f26d58cba1c164deeee15d2fb843bccca
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-01-07 16:54:42 +1300

    Update InputField styling
    - Add invalid state styling
    - Use only bottom border
    - Remove horizontal padding
    - Slightly increase border width
    - Add focus and active styling

--- client/components/input-field.jsx
+++ client/components/input-field.jsx
@@ -1,4 +1,4 @@
-
+import classNames from 'classnames';
 const InputField = ({
   placeholder,
   className,
@@ -10,12 +10,31 @@ const InputField = ({
   children,
   ...rest
 }) => {
-  return (
-    <div className="block mb-5">
-      <label htmlFor={name} className="block mb-2">{label}</label>
-      <input className={`border border-gray-300 rounded-md p-2 w-full mb-2 ${className}`} type={type} placeholder={placeholder} {...register(name, {...validations})} {...rest} />
+  {
+    /* children is the error message that has occurred for this field (see Form component) */
+  }
+  const hasValidationError = !!children;
 
-      {/* children is the error message that has occurred for this field (see Form component) */}
+  return (
+    <div className='block mb-5 group'>
+      <label
+        htmlFor={name}
+        className={classNames('block mb-2 group-focus-within:text-light-blue', {
+          'text-alert-red': hasValidationError
+        })}
+      >
+        {label}
+      </label>
+      <input
+        className={classNames(
+          `border-b-2 border-gray-300 rounded-sm py-2 w-full mb-2 outline-none active:border-light-blue-900 focus:border-light-blue-900 ${className}`,
+          { 'border-alert-red': hasValidationError }
+        )}
+        type={type}
+        placeholder={placeholder}
+        {...register(name, { ...validations })}
+        {...rest}
+      />
       {children}
     </div>
   );
--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -5,13 +5,12 @@ import Image from 'next/image';
 import { Form, InputField } from '../components';
 
 const LoginPage = ({ navigation: navigationData, footer: footerData }) => {
-
   const onSubmit = (data) => {
     console.log('Submit data: ', data);
   };
 
   return (
-    <Container className="h-[100vh]">
+    <Container className='h-[100vh]'>
       <div className='LoginPage mx-auto w-full h-full flex flex-col justify-center items-center font-poppins'>
         <div className='w-[70%] md:w-[20rem] min-h-[10rem] relative md:mb-10'>
           <Link href='/'>
@@ -22,21 +21,38 @@ const LoginPage = ({ navigation: navigationData, footer: footerData }) => {
         </div>
 
         <div className='prose flex grow md:grow-0 md:flex-shrink flex-col justify-center mb-10 w-full md:w-[60%] md:border md:border-gray-300 md:p-10 md:shadow-lg md:rounded-md'>
-          <h2 className="text-center mb-10">Member Login</h2>
+          <h2 className='text-center mb-10'>Member Login</h2>
           <Form onSubmit={onSubmit}>
-            <InputField type="text" name="emailAddress" validations={{ required: 'Please enter your email address', pattern: { value: EMAIL_REGEX, message: 'Please enter a valid email address'}}} label="Email" />
-            <InputField type="password" name="password" validations={{ required: 'Please enter your password' }} label="Password" />
-            <div className="flex justify-end mb-5">
+            <InputField
+              type='text'
+              name='emailAddress'
+              validations={{
+                required: 'Please enter your email address',
+                pattern: { value: EMAIL_REGEX, message: 'Please enter a valid email address' }
+              }}
+              label='Email'
+            />
+            <InputField
+              type='password'
+              name='password'
+              validations={{ required: 'Please enter your password' }}
+              label='Password'
+            />
+            <div className='flex justify-end mb-5'>
               <Link href='/reset-password'>
                 <a className='text-[14px] font-normal'>Forgot your password?</a>
               </Link>
             </div>
-            <InputField name="login" className="block mx-auto my-0 bg-light-blue hover:bg-lightest-blue shadow hover:text-dark-blue text-white py-2 px-4 rounded transition-colors duration-150 w-[80%] mb-5 border-none" type="submit" defaultValue="Login" />
+            <InputField
+              name='login'
+              className='block mx-auto my-0 bg-light-blue hover:bg-lightest-blue shadow hover:text-dark-blue text-white py-2 px-4 rounded transition-colors duration-150 w-[80%] mb-5 border-none'
+              type='submit'
+              defaultValue='Login'
+            />
           </Form>
         </div>
       </div>
     </Container>
-    // </Layout>
   );
 };
 
