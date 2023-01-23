fefd46d16130bda1f7b1296e50d7e4a069ae7dcd
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-01-23 15:11:37 +1300

    Add /password-reset page

---
+++ client/pages/password-reset.jsx
@@ -0,0 +1,9 @@
+export const getStaticProps = () => {
+  return { props: {} };
+};
+
+const PasswordReset = (props) => {
+  return <div>Password reset page</div>;
+};
+
+export default PasswordReset;
