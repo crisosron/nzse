913b7ddb06c421a7a5f49626cce894ecee9edccf
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-02-18 16:03:24 +1300

    Update layout of join form

--- client/components/join-page.jsx
+++ client/components/join-page.jsx
@@ -1,7 +1,8 @@
 import Container from './container';
 import InputField from './input-field';
-import { useState } from 'react';
+import { useState, Children } from 'react';
 import Form from './form';
+import { EMAIL_REGEX, PHONE_NUMBER_REGEX } from '../lib/form-utils';
 
 const DESIGNATION_OPTIONS = [
   { value: 'test-1', label: 'Test 1' },
@@ -14,6 +15,26 @@ const Section = ({ title, children }) => {
     <div className='prose bg-light-blue-50 w-full max-w-[100%] p-5 rounded-md my-10'>
       <h2>{title}</h2>
       {children}
+    </div>
+  );
+};
+
+/**
+ * This component is meant to control the width of the input fields on the form, particularly
+ * for tablet and desktop breakpoints.
+ *
+ * For mobile, the form input fields are layed out vertically, with the input fields occupying the
+ * full width of their containers, and are therefore not styled in any way by this component
+ */
+const SplitRow = ({ children }) => {
+  const singleChild = Children.toArray(children).length === 1;
+
+  return (
+    // Note that the children selector &>div etc. targets InputField components. If the container
+    // Of an input field changes to a different element, the selector here should be updated as well
+    <div className='md:flex md:justify-between md:[&>div]:w-[100%] md:first:[&>div]:mr-5'>
+      {children}
+      {singleChild && <div className='hidden lg:block'></div>}
     </div>
   );
 };
@@ -32,24 +53,42 @@ const JoinPage = () => {
       richtext)
       <Form onSubmit={onSubmit}>
         <Section title='Your Details'>
-          <InputField
-            type='text'
-            name='firstName'
-            validations={{ required: 'Please enter your first name ' }}
-            label='First name'
-          />
-          <InputField
-            type='text'
-            name='surname'
-            validations={{ required: 'Please enter your surname ' }}
-            label='Surname'
-          />
-          <InputField
-            type='date'
-            name='dob'
-            validations={{ required: 'Please enter your date of birth ' }}
-            label='Date of birth'
-          />
+          <SplitRow>
+            <InputField
+              type='text'
+              className='w-[100%]'
+              name='firstName'
+              validations={{ required: 'Please enter your first name ' }}
+              label='First name'
+            />
+            <InputField
+              type='text'
+              name='surname'
+              validations={{ required: 'Please enter your surname ' }}
+              label='Surname'
+            />
+          </SplitRow>
+          <SplitRow>
+            <InputField
+              type='date'
+              name='dob'
+              validations={{ required: 'Please enter your date of birth ' }}
+              label='Date of birth'
+            />
+          </SplitRow>
+          <SplitRow>
+            <InputField
+              type='tel'
+              name='mobileNumber'
+              validations={{
+                pattern: {
+                  value: PHONE_NUMBER_REGEX,
+                  message: 'Please enter a valid NZ mobile phone number (should start with 02)'
+                }
+              }}
+              label='Mobile number'
+            />
+          </SplitRow>
         </Section>
 
         <Section title='Address'>
@@ -59,30 +98,38 @@ const JoinPage = () => {
             validations={{ required: 'Please enter your address' }}
             label='Street number and name'
           />
-          <InputField type='text' name='suburb' label='Suburb' />
-          <InputField type='text' name='city' label='City' />
-          <InputField
-            type='number'
-            name='postcode'
-            validations={{
-              required: 'Please enter your postcode',
-              minLength: { value: 4, message: 'Please enter a valid postcode ' },
-              maxLength: { value: 4, message: 'Please enter a valid postcode ' }
-            }}
-            label='Post code'
-          />
+          <SplitRow>
+            <InputField type='text' name='suburb' label='Suburb' />
+            <InputField type='text' name='city' label='City' />
+          </SplitRow>
+          <SplitRow>
+            <InputField
+              type='number'
+              name='postcode'
+              validations={{
+                required: 'Please enter your postcode',
+                minLength: { value: 4, message: 'Please enter a valid postcode ' },
+                maxLength: { value: 4, message: 'Please enter a valid postcode ' }
+              }}
+              label='Post code'
+            />
+          </SplitRow>
         </Section>
 
         <Section title='Your professional details'>
-          <InputField type='text' name='institution' label='Institution' />
-          <InputField type='text' name='department' label='Department' />
-          <InputField
-            type='select'
-            name='designation'
-            label='Designation'
-            placeholder='Select a designation'
-            options={DESIGNATION_OPTIONS}
-          />
+          <SplitRow>
+            <InputField type='text' name='institution' label='Institution' />
+            <InputField type='text' name='department' label='Department' />
+          </SplitRow>
+          <SplitRow>
+            <InputField
+              type='select'
+              name='designation'
+              label='Designation'
+              placeholder='Select a designation'
+              options={DESIGNATION_OPTIONS}
+            />
+          </SplitRow>
         </Section>
 
         <Section title='Creating your account'>
@@ -90,24 +137,31 @@ const JoinPage = () => {
             Please enter your email address and a password to setup your account. Your account will
             be activated after successful payment.
           </p>
-          <InputField
-            type='text'
-            name='emailAddress'
-            validations={{ required: 'Please enter your email address' }}
-            label='Email'
-          />
-          <InputField
-            type='text'
-            name='password'
-            validations={{ required: 'Please enter your password' }}
-            label='Password'
-          />
-          <InputField
-            type='text'
-            name='confirmPassword'
-            validations={{ required: 'Please re-enter your password' }}
-            label='Confirm password'
-          />
+          <SplitRow>
+            <InputField
+              type='email'
+              name='emailAddress'
+              validations={{
+                required: 'Please enter your email address',
+                pattern: { value: EMAIL_REGEX, message: 'Please enter a valid email address' }
+              }}
+              label='Email'
+            />
+          </SplitRow>
+          <SplitRow>
+            <InputField
+              type='password'
+              name='password'
+              validations={{ required: 'Please enter a password' }}
+              label='Password'
+            />
+            <InputField
+              type='password'
+              name='confirmPassword'
+              validations={{ required: 'Please re-enter your chosen password' }}
+              label='Confirm password'
+            />
+          </SplitRow>
         </Section>
 
         <Section title='Declaration and payment'>
@@ -118,7 +172,7 @@ const JoinPage = () => {
             validations={{ required: 'Please accept the terms and conditions to continue' }}
           >
             <span>
-              By ticking, you are confirming that you have read, understood and agree to our{' '}
+              By ticking, you are confirming that you have read, understood, and agree to our{' '}
               <a>terms and conditions</a>.
             </span>
           </InputField>
