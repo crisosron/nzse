3c68dbc859abf3f0c2ec52a127ebf6ba43b72129
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-03-15 18:58:30 +1300

    Tweak CardBlocks styling

--- client/components/blocks/card-blocks/card-blocks.jsx
+++ client/components/blocks/card-blocks/card-blocks.jsx
@@ -2,7 +2,7 @@ import CardBlock from '../card-block';
 
 const CardBlocks = ({ cardBlocks }) => {
   return (
-    <div className='flex flex-col lg:flex-row'>
+    <div className='flex flex-col lg:flex-row lg:justify-center'>
       {cardBlocks.length &&
         cardBlocks.map((cardBlock, index) => {
           return <CardBlock key={`card-block-${index}`} {...cardBlock} />;
