b1fbc29a05c85e5625bc54f71fe40fcffa1f4b5f
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-03-19 17:36:45 +1300

    Add handling of member activation and pending member deletion on webhook receive

--- client/pages/api/stripe/event-webhook.js
+++ client/pages/api/stripe/event-webhook.js
@@ -1,10 +1,57 @@
 import { initStripe } from "../../../lib/stripe";
 import { buffer } from "micro";
+import { activateMember } from "../members/activate-member";
+import { deletePendingMember } from "../members/delete-pending-member";
 
-// Disable the body parser for this api route because we need to send the raw body to Stripe
+// Disable the body parser for this api route because we need to send the raw body to Stripe for
+// verification
 export const config = {
   api: {
     bodyParser: false,
+  }
+};
+
+const findStripeCustomerById = async (customerId) => {
+  const stripe = await initStripe();
+  const stripeCustomer = await stripe.customers.retrieve(customerId);
+  return stripeCustomer;
+};
+
+// Activate the firebase account of the customer in the charge object
+const activatePendingMemberAccount = async (charge) => {
+  const { customer: customerId } = charge;
+  const customerObject = await findStripeCustomerById(customerId);
+  const { email: customerEmail } = customerObject;
+
+  if(!customerEmail) {
+    throw new Error('No email address was found in the customer stripe object. Cannot activate member');
+  }
+
+  const activationResult = await activateMember(customerEmail);
+
+  if(activationResult.error) {
+    throw new Error('Failed to activate member: ', activationResult.error.message);
+  }
+};
+
+// Delete the firebase account of the customer in the charge object.
+//
+// Deleting the firebase account is important to free up the email address in firebase, ensuring that
+// subsequent attempts at registering with the same email address is not blocked by 'email already
+// exists' error in firebase
+const deletePendingMemberAccount = async (charge) => {
+  const { customer: customerId } = charge;
+  const customerObject = await findStripeCustomerById(customerId);
+  const { email: customerEmail } = customerObject;
+
+  if(!customerEmail) {
+    throw new Error('No email address was found in the customer stripe object. Cannot activate member');
+  }
+
+  const deletionResult = await deletePendingMember(customerEmail);
+
+  if(deletionResult.error) {
+    throw new Error('Failed to activate member: ', deletionResult.error.message);
   }
 };
 
@@ -28,7 +75,25 @@ export default async function handler(req, res) {
     return;
   }
   
+  // Return 200 here so that the webhook doesn't timeout
   res.status(200).json({ received: true });
 
-  console.log('event: ', event);
+  try {
+    switch (event.type) {
+      case 'charge.captured':
+        activatePendingMemberAccount(event.data.object);
+        break;
+      case 'charge.refunded':
+        deletePendingMemberAccount(event.data.object);
+        break;
+      case 'charge.expired':
+        deletePendingMemberAccount(event.data.object);
+        break;
+      case 'charge.failed':
+        deletePendingMemberAccount(event.data.object);
+        break;
+    }
+  } catch(error) {
+    console.log('Error handling webhook event: ', error.message);
+  }
 }
