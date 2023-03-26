c5c405accfe5329740749fda8b21bb723274e43c
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-03-26 16:25:30 +1300

    Fix sidebar link URL resolution

--- client/components/blocks/sidebar-link-block/sidebar-link-block.jsx
+++ client/components/blocks/sidebar-link-block/sidebar-link-block.jsx
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 const SidebarLinkBlock = (props) => {
-  // TODO: Get users logged in status somehow...
   const { sidebarLinkTitle: title, page, className } = props;
   const {
     title: pageTitle,
@@ -12,12 +11,12 @@ const SidebarLinkBlock = (props) => {
   } = unwrapEntityResponse(page);
 
   const router = useRouter();
-  const typeRoute = pageType === 'Patients' ? 'patients' : 'professionals';
+  const href = pageType.toLowerCase() === 'root' ? `/${pageSlug}` : `/${pageType.toLowerCase()}/${pageSlug}`;
   const pathComponents = router.asPath.split('/');
   const representsCurrentRoute = pathComponents[pathComponents.length - 1] === pageSlug;
 
   return (
-    <Link href={`/${typeRoute}/${pageSlug}`}>
+    <Link href={href}>
       <a
         className={`block mb-4 ${
           representsCurrentRoute ? 'text-light-blue-700' : 'text-gray'
