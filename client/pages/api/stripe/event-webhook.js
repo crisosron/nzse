ed51bc7d36d513db63fb87de5a49db8ebb60b6d8
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-03-29 23:00:04 +1300

    Remove console logs

--- client/pages/api/stripe/event-webhook.js
+++ client/pages/api/stripe/event-webhook.js
@@ -1,5 +1,4 @@
 import { initStripe } from "../../../lib/stripe";
-// import { buffer } from "micro";
 import { activateMember } from "../members/activate-member";
 import { deletePendingMember } from "../members/delete-pending-member";
 
@@ -44,14 +43,12 @@ const deletePendingMemberAccount = async (charge) => {
   const { customer: customerId } = charge;
   const customerObject = await findStripeCustomerById(customerId);
   const { email: customerEmail } = customerObject;
-  console.log('deleting acc with email: ', customerEmail);
 
   if(!customerEmail) {
     throw new Error('No email address was found in the customer stripe object. Cannot activate member');
   }
 
   const deletionResult = await deletePendingMember(customerEmail);
-  console.log('Deletion result: ', deletionResult);
 
   if(deletionResult.error) {
     throw new Error('Failed to activate member: ', deletionResult.error.message);
@@ -75,17 +72,9 @@ export default async function handler(req, res) {
   }
 
   const stripe = await initStripe();
-  // const bodyBuffer = await buffer(req);
   const buf = await buffer(req);
   const rawBody = buf.toString('utf8');
   const signature = req.headers['stripe-signature'];
-
-  // TODO: add console logs to deletion and activation fns above to see if they are triggering
-  // console.log('req.body: ', req.body);
-  // console.log('req.rawBody: ', req.rawBody);
-  // console.log('rawBody: ', rawBody);
-
-  // const body = process.env.NODE_ENV === 'development' ? bodyBuffer : req.body;
 
   let event;
 
@@ -96,25 +85,18 @@ export default async function handler(req, res) {
     return;
   }
   
-  // Return 200 here so that the webhook doesn't timeout
-  // res.status(200).json({ received: true });
-  console.log('event.type: ', event.type);
-
   try {
     switch (event.type) {
       case 'charge.captured':
         await activatePendingMemberAccount(event.data.object);
         break;
       case 'charge.refunded':
-        console.log('HANDLING charge.refunded event');
         await deletePendingMemberAccount(event.data.object);
         break;
       case 'charge.expired':
-        console.log('HANDLING charge.expired event');
         await deletePendingMemberAccount(event.data.object);
         break;
       case 'charge.failed':
-        console.log('HANDLING charge.failed event');
         await deletePendingMemberAccount(event.data.object);
         break;
     }
