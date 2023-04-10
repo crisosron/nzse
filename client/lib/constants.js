55810dbb0e19e01560782ebd0871da16a79b5044
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-04-10 15:58:01 +1200

    Move firebase error code to prose mapping to constants

--- client/lib/constants.js
+++ client/lib/constants.js
@@ -2,3 +2,9 @@ export const PAGE_LINKS = {
   MEMBERSHIP_INFO: '/professionals/join-nzse',
   PASSWORD_RESET: '/password-reset'
 };
+
+// Maps an error code returned by firebase to prose
+// All error codes and what they mean: https://firebase.google.com/docs/auth/admin/errors
+export const FIREBASE_ERROR_MESSAGE = {
+  'auth/user-not-found': 'A user with that email address could not be found.'
+};
