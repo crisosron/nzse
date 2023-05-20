afabac4212df2fbb0c3dee6bec78ea2677d5da00
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-05-20 23:37:53 +1200

    Don't use ApolloLink?
    Try to remove ApolloLink to see if that fixes the build error on
    Vercel

--- client/lib/graphql-api.js
+++ client/lib/graphql-api.js
@@ -5,20 +5,20 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 const GRAPHQL_API_URL =
   process.env.NEXT_PUBLIC_STRAPI_GRAPHQL_API_URL || 'http://strapi:1337/graphql';
 
-const errorLink = onError(({ graphqlErrors, networkError }) => {
-  if (graphqlErrors) {
-    console.error('graphqlErrors: ', graphqlErrors);
-    console.log('Graphql Error Details');
-    graphqlErrors.forEach((error) => {
-      console.log('message: ', error.message);
-      console.log('extensions: ', error.extensions);
-    });
-  }
-  if (networkError) console.error('networkError: ', networkError);
-});
+// const errorLink = onError(({ graphqlErrors, networkError }) => {
+//   if (graphqlErrors) {
+//     console.error('graphqlErrors: ', graphqlErrors);
+//     console.log('Graphql Error Details');
+//     graphqlErrors.forEach((error) => {
+//       console.log('message: ', error.message);
+//       console.log('extensions: ', error.extensions);
+//     });
+//   }
+//   if (networkError) console.error('networkError: ', networkError);
+// });
 
-const httpLink = new HttpLink({ uri: GRAPHQL_API_URL });
-const link = ApolloLink.from([errorLink, httpLink]);
+// const httpLink = new HttpLink({ uri: GRAPHQL_API_URL });
+// const link = ApolloLink.from([errorLink, httpLink]);
 
 const graphqlClient = new ApolloClient({
   uri: GRAPHQL_API_URL,
@@ -31,7 +31,7 @@ const graphqlClient = new ApolloClient({
       fetchPolicy: 'network-only'
     }
   },
-  link
+  // link
 });
 
 export { graphqlClient };
