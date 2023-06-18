5b690fde25d5ded997f74ebeac914cdbaeb636f9
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-06-18 15:52:59 +1200

    Don't use apollo-link-error?

--- client/lib/graphql-api.js
+++ client/lib/graphql-api.js
@@ -1,24 +1,24 @@
-import { onError } from 'apollo-link-error';
+// import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const GRAPHQL_API_URL =
   process.env.NEXT_PUBLIC_STRAPI_GRAPHQL_API_URL || 'http://strapi:1337/graphql';
 
-const errorLink = onError(({ graphqlErrors, networkError }) => {
-  if (graphqlErrors) {
-    console.error('graphqlErrors: ', graphqlErrors);
-    console.log('Graphql Error Details');
-    graphqlErrors.forEach((error) => {
-      console.log('message: ', error.message);
-      console.log('extensions: ', error.extensions);
-    });
-  }
-  if (networkError) console.error('networkError: ', networkError);
-});
+// const errorLink = onError(({ graphqlErrors, networkError }) => {
+//   if (graphqlErrors) {
+//     console.error('graphqlErrors: ', graphqlErrors);
+//     console.log('Graphql Error Details');
+//     graphqlErrors.forEach((error) => {
+//       console.log('message: ', error.message);
+//       console.log('extensions: ', error.extensions);
+//     });
+//   }
+//   if (networkError) console.error('networkError: ', networkError);
+// });
 
 const httpLink = new HttpLink({ uri: GRAPHQL_API_URL });
-const link = ApolloLink.from([errorLink, httpLink]);
+const link = ApolloLink.from([httpLink]);
 
 const graphqlClient = new ApolloClient({
   uri: GRAPHQL_API_URL,
