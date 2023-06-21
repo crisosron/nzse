9c7ec47e21400b1290b3eda7a91f5d5dd7e462f7
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-06-21 21:52:27 +1200

    Add console logs

--- client/lib/general-page-utils.js
+++ client/lib/general-page-utils.js
@@ -5,11 +5,12 @@ import {
 } from '../graphql/queries';
 import { unwrapCollectionEntityResponse } from './utils';
 
-// Used for getStaticPaths
-// Note: getStaticProps is not used anymore in favor of getServersideProps 
-// but keep this for future reference
 const buildGeneralPageSlugs = async (pageType) => {
+  console.log('buildGeneralPageSlugs: ', pageType);
   const slugQuery = buildGeneralPageSlugsByTypeQuery(pageType);
+  const slugQueryResults = await graphqlClientURQL.query(slugQuery).toPromise();
+  console.log('slugQueryResults: ');
+  console.dir(slugQueryResults, { depth: null });
   const slugs = unwrapCollectionEntityResponse(
     // await graphqlClient.query({ query: slugQuery }),
     await graphqlClientURQL.query(slugQuery).toPromise(),
@@ -19,6 +20,8 @@ const buildGeneralPageSlugs = async (pageType) => {
     return `/${pageType.toLowerCase()}/${entry.slug}`;
   });
 
+  console.log('Got slugs: ', slugs);
+
   return slugs;
 };
 
@@ -27,6 +30,8 @@ const buildGeneralPageProps = async (params, pageType) => {
   const query = buildGeneralPageBySlugAndTypeQuery(slug, pageType);
   // const queryResponse = await graphqlClient.query({ query });
   const queryResponse = await graphqlClientURQL.query(query).toPromise();
+  console.log('queryResponse: ');
+  console.dir(queryResponse, { depth: null });
   const generalPages = unwrapCollectionEntityResponse(queryResponse, 'generalPages');
   return generalPages.length ? generalPages[0] : null;
 };
