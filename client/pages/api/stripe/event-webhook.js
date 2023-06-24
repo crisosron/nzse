8a3f1103c358512a725897aabbf76203b159b2ac
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-06-24 19:10:09 +1200

    Use env var

--- client/pages/api/stripe/event-webhook.js
+++ client/pages/api/stripe/event-webhook.js
@@ -60,7 +60,6 @@ const deactivateMemberLogin = async (customerId) => {
 // pay for their membership, an invoice is sent with instructions on how to pay, instead of
 // automatically charging the user's payment method
 const handleSubscriptionCreate = async (subscription) => {
-  console.log('HANDLE SUBSCRIPTION CREATE CALLED');
   const stripe = await initStripe();
   const { id } = subscription;
   
@@ -170,7 +169,7 @@ export default async function handler(req, res) {
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(rawBody, signature, 'whsec_d94cdf906ce5cfbce8fb03ddb288a82ef750a970efa6c493a2c56e2cc7696e48');
+    event = stripe.webhooks.constructEvent(rawBody, signature, process.env.STRIPE_WEBHOOK_SECRET_KEY);
   } catch (error) {
     res.status(400).send(`Webhook Error: ${error.message}`);
     return;
