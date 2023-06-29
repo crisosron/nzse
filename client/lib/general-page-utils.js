73f989b4b9cb76657c9e7081901383145f05616d
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-06-29 22:38:06 +1200

    Add fetching for root general pages

--- client/lib/general-page-utils.js
+++ client/lib/general-page-utils.js
@@ -1,4 +1,4 @@
-import { graphqlClientURQL } from './graphql-api';
+import { graphqlClient } from './graphql-api';
 import {
   buildGeneralPageBySlugAndTypeQuery,
   buildGeneralPageSlugsByTypeQuery
@@ -8,12 +8,8 @@ import { unwrapCollectionEntityResponse } from './utils';
 const buildGeneralPageSlugs = async (pageType) => {
   console.log('buildGeneralPageSlugs: ', pageType);
   const slugQuery = buildGeneralPageSlugsByTypeQuery(pageType);
-  const slugQueryResults = await graphqlClientURQL.query(slugQuery).toPromise();
-  console.log('slugQueryResults: ');
-  console.dir(slugQueryResults, { depth: null });
   const slugs = unwrapCollectionEntityResponse(
-    // await graphqlClient.query({ query: slugQuery }),
-    await graphqlClientURQL.query(slugQuery).toPromise(),
+    await graphqlClient.query({ query: slugQuery }),
     'generalPages'
   ).map((entry) => {
     if (pageType === 'Root') return `/${entry.slug}`;
@@ -28,10 +24,9 @@ const buildGeneralPageSlugs = async (pageType) => {
 const buildGeneralPageProps = async (params, pageType) => {
   const { slug } = params;
   const query = buildGeneralPageBySlugAndTypeQuery(slug, pageType);
-  // const queryResponse = await graphqlClient.query({ query });
-  const queryResponse = await graphqlClientURQL.query(query).toPromise();
-  console.log('queryResponse: ');
-  console.dir(queryResponse, { depth: null });
+  const queryResponse = await graphqlClient.query({ query });
+  // console.log('queryResponse: ');
+  // console.dir(queryResponse, { depth: null });
   const generalPages = unwrapCollectionEntityResponse(queryResponse, 'generalPages');
   return generalPages.length ? generalPages[0] : null;
 };
--- client/pages/[...slug].jsx
+++ client/pages/[...slug].jsx
@@ -1,31 +1,31 @@
 import { GeneralPage } from '../components';
-// import { buildGeneralPageSlugs, buildGeneralPageProps } from '../lib/general-page-utils';
+import { buildGeneralPageSlugs, buildGeneralPageProps } from '../lib/general-page-utils';
 
-// export const getStaticPaths = async () => {
-//   return {
-//     paths: await buildGeneralPageSlugs('Root'),
-//     fallback: 'blocking'
-//   };
-// };
+export const getStaticPaths = async () => {
+  return {
+    paths: await buildGeneralPageSlugs('Root'),
+    fallback: 'blocking'
+  };
+};
 
-// export const getStaticProps = async (context) => {
-//   const { params } = context;
-//   const props = await buildGeneralPageProps(params, 'Root');
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  const props = await buildGeneralPageProps(params, 'Root');
 
-//   if (!props) {
-//     return {
-//       notFound: true
-//     };
-//   }
+  if (!props) {
+    return {
+      notFound: true
+    };
+  }
 
-//   return {
-//     props: {
-//       ...props,
-//     },
+  return {
+    props: {
+      ...props,
+    },
 
-//     revalidate: 60
-//   };
-// };
+    revalidate: 60
+  };
+};
 
 const RootGeneralPage = (props) => {
   // const { sidebar } = props;
