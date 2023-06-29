40ce5c7e6c33d3d77345dfdc4a68c77f485e2b31
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-06-29 22:46:48 +1200

    Add rendering of homepage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,52 +1,37 @@
-// import { useContext } from 'react';
-// import Layout from '../components/layout';
-// import { GlobalContext } from '../lib/contexts/global-context';
-// import { graphqlClientURQL } from '../lib/graphql-api';
-// import { getHomepage } from '../graphql/queries';
-// import { Blocks } from '../components/blocks';
+import { useContext } from 'react';
+import Layout from '../components/layout';
+import { GlobalContext } from '../lib/contexts/global-context';
+import { graphqlClient } from '../lib/graphql-api';
+import { getHomepage } from '../graphql/queries';
+import { Blocks } from '../components/blocks';
 
-// const Home = ({ homepage }) => {
-//   const {
-//     footer: footerData,
-//     navigation: navigationData
-//   } = useContext(GlobalContext);
+const Home = ({ homepage }) => {
+  const {
+    footer: footerData,
+    navigation: navigationData
+  } = useContext(GlobalContext);
 
-//   const { blocks: homepageBlocks } = homepage;
-//   return (
-//     <Layout footerData={footerData} navigationData={navigationData}>
-//       <Blocks blocks={homepageBlocks} />
-//     </Layout>
-//   );
-// };
-
-const Home = () => {
+  const { blocks: homepageBlocks } = homepage;
   return (
-    <div>Homepage</div>
+    <Layout footerData={footerData} navigationData={navigationData}>
+      <Blocks blocks={homepageBlocks} />
+    </Layout>
   );
 };
 
 export const getStaticProps = async () => {
-  // const [{ data: homepageData }] = await Promise.all([
-  //   graphqlClient.query({ query: getHomepage })
-  // ]);
-  // const [{ data: homepageData }] = await Promise.all([
-  //   graphqlClientURQL.query(getHomepage).toPromise()
-  // ]);
+  const [{ data: homepageData }] = await Promise.all([
+    graphqlClient.query({ query: getHomepage })
+  ]);
 
-  // console.log('homepageData: ', homepageData);
-
-  // const homepage = homepageData.homepage.data?.attributes;
-
-  // return {
-  //   props: {
-  //     homepage,
-  //   },
-
-  //   revalidate: 60
-  // };
+  const homepage = homepageData.homepage.data?.attributes;
 
   return {
-    props: {}
+    props: {
+      homepage,
+    },
+
+    revalidate: 60
   };
 };
 
